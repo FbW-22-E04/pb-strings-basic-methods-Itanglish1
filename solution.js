0f229490ffc3a66cd9757946f6ff23b2113e978a
chris <chrishethertonweb@gmail.com>
2022-07-04 11:55:17 +0200

    up to exercise 3

--- solution.js
+++ solution.js
@@ -1,2 +1,10 @@
 let parkWalkStr = "I can walk in the park all day!";
 console.log(parkWalkStr.slice(18, 22));
+
+let helloStr = "Hello World";
+let helloStrUp = helloStr.toUpperCase();
+console.log(helloStrUp);
+
+let earthlingsStr = "Hello Earthling";
+let earthlingsStrLow = earthlingsStr.toLowerCase();
+console.log(earthlingsStrLow);
